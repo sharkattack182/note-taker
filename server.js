7b4718b874b753d36980c6f9a19b438a1c01d0a0
Matthew Courtney <matthewdc182@gmail.com>
2020-09-03 00:16:29 -0400

    got all of the errors out and got the post working

--- server.js
+++ server.js
@@ -1,59 +1,50 @@
-// need to run requirements
+
 const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
-const path = require("paths");
-const dbj = require("db.json");
+const path = require("path");
 const fs = require("fs");
+let array = [];
 
 
 
-//set up the server port
 const app = express();
 const PORT = process.env.PORT || 5501;
 
-//parse data
+app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extend: true}));
 app.use(express.static("public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes)
+
 
 //app.get functions/ send files
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public", "/index.html"));
 })
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname+ "/public", "/notes.html"));
 })
 
-app.get("/api/notes", function(req, res) {
-    res.json(dbj);
-  });
+app.get("/api/notes", function (req, res) {
+    return res.json(JSON.parse(array));
+});
 
+fs.readFile(__dirname + "/db.json", "utf-8", function (er, data) {
+    if (er) throw er;
+    array.push(data)
+})
 
-  app.post("/api/notes", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    var newNote = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newNote.id = Math.floor(Math.random() * 10000)
-  
-    console.log(newNote);
-  
-    dbj.push(newNote);
-  
-    res.json(newNote);
+app.post("/api/notes", function (req, res) {
 
-    fs.writeFile("db.json", JSON.stringify([dbj]), "utf-8", function(er) {
-        if(er) throw er;
-        console.log("successfully added note")
+    req.body.id = Math.floor(Math.random() * 10000);
+    array = JSON.parse(array)
+    array.push(req.body);
+    array = JSON.stringify(array);
+
+    fs.writeFile(__dirname + "/db.json", array, "utf-8", function (er) {
+        if (er) throw er;
     })
-  });
-  
+    res.json(JSON.parse(array))
+});
+
 
 // start the server on the port (this was in the starting point instructor gave dourung office hours)
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
