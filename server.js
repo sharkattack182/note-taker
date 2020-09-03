4ae10b89b4e4af84d1b2cbd2de0590446ec736c3
Matthew Courtney <matthewdc182@gmail.com>
2020-09-02 22:44:45 -0400

    got the server running and returning info from db.json

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const path = require("paths");
+const dbj = require("db.json")
 
 
 
 //set up the server port
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5501;
 
 //parse data
 app.use(express.json());
@@ -19,13 +20,16 @@ app.use("/", htmlRoutes)
 
 //app.get functions/ send files
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 })
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 })
 
+app.get("/api/notes", function(req, res) {
+    res.json(dbj);
+  });
 //display notes
 
 // start the server on the port (this was in the starting point instructor gave dourung office hours)
