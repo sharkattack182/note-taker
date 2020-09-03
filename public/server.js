479ed2413f83b6bdfa5f684aff7fbeab8d208fe8
Matthew Courtney <matthewdc182@gmail.com>
2020-09-02 21:49:47 -0400

    added om the route methods

--- public/server.js
+++ public/server.js
@@ -2,11 +2,13 @@
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("paths");
+
 
 
 //set up the server port
 const app = express();
-const PORT = pricess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 //parse data
 app.use(express.json());
@@ -16,7 +18,17 @@ app.use("/api", apiRoutes);
 app.use("/", htmlRoutes)
 
 //app.get functions/ send files
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+})
+
+app.get("/index.js", function(req, res) {
+    res.sendFile(path.join(__dirname, "assets/js/index.js"));
+})
 //display notes
 
 // start the server on the port (this was in the starting point instructor gave dourung office hours)
