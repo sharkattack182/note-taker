def8e8196fc319da06574ee5d11789f7c113f121
Matthew Courtney <matthewdc182@gmail.com>
2020-09-02 22:57:24 -0400

    added in the ability to create a new note

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const path = require("paths");
-const dbj = require("db.json")
+const dbj = require("db.json");
+const fs = require("fs");
 
 
 
@@ -30,7 +31,29 @@ app.get("/notes", function(req, res) {
 app.get("/api/notes", function(req, res) {
     res.json(dbj);
   });
-//display notes
+
+
+  app.post("/api/notes", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newNote = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newNote.id = Math.floor(Math.random() * 10000)
+  
+    console.log(newNote);
+  
+    dbj.push(newNote);
+  
+    res.json(newNote);
+
+    fs.writeFile("db.json", JSON.stringify([dbj]), "utf-8", function(er) {
+        if(er) throw er;
+        console.log("successfully added note")
+    })
+  });
+  
 
 // start the server on the port (this was in the starting point instructor gave dourung office hours)
 app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
